de80381d32f72e39e53a7266c235d541f5c3a431
wuhailei <wuhailei1989@gmail.com>
2015-09-24 13:43:43 +0800

    website update

--- sites/settings.py
+++ sites/settings.py
@@ -12,20 +12,26 @@ ADMINS = (
 
 MANAGERS = ADMINS
 DATABASES = {
+    # 'default': {
+    #     #'ENGINE': 'django_mysqlpool.backends.mysqlpool',
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     #'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+    #     #'NAME': '/data/garden/db/garden.db',                      # Or path to database file if using sqlite3.
+    #     # The following settings are not used with sqlite3:
+    #     'USER': 'garden',
+    #     'PASSWORD': 'garden123',
+    #     'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+    #     'PORT': '3306', # Set to empty string for default.
+    #     'NAME': 'garden',
+    #     'OPTIONS': {
+    #         'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
+    #     }
+    # }
     'default': {
-        #'ENGINE': 'django_mysqlpool.backends.mysqlpool',
-        'ENGINE': 'django.db.backends.mysql',
-        #'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        #'NAME': '/data/garden/db/garden.db',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/data/garden/db/garden.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'garden',
-        'PASSWORD': 'garden123',
-        'HOST': 'localhost', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306', # Set to empty string for default.
-        'NAME': 'garden',
-        'OPTIONS': {
-            'init_command': 'SET storage_engine=INNODB,character_set_connection=utf8,collation_connection=utf8_unicode_ci'
-        }
+        
     }
 }
 DOMAIN = 'localhost'
